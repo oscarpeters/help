da3c241ca638d9a541d53e47fdef1bda30c012e6
Oscar <oscarpeters@live.nl>
2024-07-26 20:10:42 +0200

    update

--- 01. Main Files/assets/js/textrotate.js
+++ 01. Main Files/assets/js/textrotate.js
@@ -9,36 +9,40 @@ var TxtRotate = function(el, toRotate, period) {
 };
 
 TxtRotate.prototype.tick = function() {
-  var i = this.loopNum % this.toRotate.length; // go to next word on incrementing i
+  var i = this.loopNum % this.toRotate.length;
   var fullTxt = this.toRotate[i];
-  
-  // Check deleting status, either adding or removing text
+
+  // Check if we need to add a newline based on screen width
+  if (window.innerWidth < 500 && !fullTxt.startsWith('\n')) {
+    fullTxt = '\n' + fullTxt;
+    this.toRotate[i] = fullTxt; // update the array to prevent adding newline again
+  }
+
   if (this.isDeleting) {
     this.txt = fullTxt.substring(0, this.txt.length - 1);
   } else {
     this.txt = fullTxt.substring(0, this.txt.length + 1);
   }
-  
-  // Actually setting the text
-  this.el.innerHTML = '<span class="wrap" style="color: aliceblue;">'+ this.txt +'</span>';
-  
-  // Misc variables
+
+  // Handle newline character for display
+  var displayTxt = this.txt.replace(/\n/g, '<br>');
+
+  this.el.innerHTML = '<span class="wrap" style="color: aliceblue;">' + displayTxt + '</span>';
+
   var that = this;
   var delta = 95 - Math.random() * 40;
-  
-  // Delete twice as fast
+
   if (this.isDeleting) { delta /= 2; }
 
-  if (!this.isDeleting && this.txt === fullTxt) { /* if all typed */
-    delta = this.period; /* wait for this.period */
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
     this.isDeleting = true;
-  } else if (this.isDeleting && this.txt === '') { /* if all deleted */
+  } else if (this.isDeleting && this.txt === '') {
     this.isDeleting = false;
-    this.loopNum++; // next word
-    delta = 500; /* wait for a set 500ms */
+    this.loopNum++;
+    delta = 500;
   }
-  
-  // Start the loop again
+
   setTimeout(function() {
     that.tick();
   }, delta);
@@ -46,7 +50,7 @@ TxtRotate.prototype.tick = function() {
 
 window.onload = function() {
   var element = document.getElementById('txt-rotate');
-  var toRotate = element.getAttribute('data-rotate'); // truthy if not blank
+  var toRotate = element.getAttribute('data-rotate');
   var period = element.getAttribute('data-period');
   if (toRotate) {
     new TxtRotate(element, JSON.parse(toRotate), period);
